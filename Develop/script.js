e59c61d94fa93a5b5d0f3b894d3d84bc01ea64d6
n.alipour777@yahoo.com <n.alipour777@yahoo.com>
2023-03-03 17:29:14 +1100

    add array for numeric, lowercase and uppercase characters

--- Develop/script.js
+++ Develop/script.js
@@ -43,7 +43,7 @@ generateBtn.addEventListener("click", writePassword);
 // we need IF to say :
       //if i=4 --> window.alert(At least one charater type must be selected)
 
-var specialCharacter= [
+var specialCharacters = [
   "!",
   "#",
   "$",
@@ -73,4 +73,21 @@ var specialCharacter= [
   "|",
   "}",
   "~",
-]
+];
+
+var numericCharacters = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9"
+];
+
+var lowerCaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
