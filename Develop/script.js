a7d94eff87a169828826500c41d0410dab0a668a
n.alipour777@yahoo.com <n.alipour777@yahoo.com>
2023-03-04 02:00:37 +1100

    move the random choice of variables inside the while

--- Develop/script.js
+++ Develop/script.js
@@ -7,16 +7,16 @@ var numericCharacters = ["0","1","2","3","4","5","6","7","8","9"];
 var lowerCaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var cancelationNumber = 0;
-var passwordLenght;
-var password = [];
+var passwordLength;
 
 function generatePassword () {
-  var passwordLenght = prompt ("How many charater would you like your password to contain?", " ")
-  if (passwordLenght === null) {
-  return; 
-  } else if ((passwordLenght<8) || (passwordLenght>128)) {
-  window.alert ("Password length must be between 8 to 128 charater.");
-  };
+  var passwordLength = prompt ("How many charater would you like your password to contain?", " ")
+  if (passwordLength === null) {
+    return; 
+  } else if ((passwordLength < 8) || (passwordLength > 128)) {
+    window.alert ("Password length must be between 8 to 128 charater.");
+    return;
+  }
 
   var special = confirm("Click Ok to confirm your password including special character");
   var numeric = confirm("Click Ok to confirm your password including numeric character");
@@ -27,52 +27,34 @@ function generatePassword () {
       window.alert("At least one charater type must be selected")
     };
 
-  For (password.length < passwordLenght) {
+  var password = [];
+
+  while (password.length < passwordLength) {
     if (special) {
-      function specialCharacterFunction ();
+      var randomIndex = Math.floor(Math.random() * specialCharacters.length);
+      randomElement = specialCharacters [randomIndex];
+      password=password.unshift(randomElement);
     }
     if (numeric) {
-      function numericCharacterFunction ();
+      var randomIndex = Math.floor(Math.random() * numericCharacters.length);
+      randomElement = numericCharacters [randomIndex];
+      password=password.unshift(randomElement);
     }
     if (lowerCase) {
-      function lowerCaseCharacterFunction ();
+      var randomIndex = Math.floor(Math.random() * lowerCaseCharacters.length);
+      randomElement = lowerCaseCharacters [randomIndex];
+      password=password.unshift(randomElement);
     }
     if (upperCase) {
-      function upperCaseCharacterFunction ();
+      var randomIndex = Math.floor(Math.random() * upperCaseCharacters.length);
+      randomElement = upperCaseCharacters [randomIndex];
+      password=password.unshift(randomElement);
     }
   }
 
 };
 
-function specialCharacterFunction () {
-  var randomIndex = Math.floor(Math.random() * specialCharacters.length);
-  randomElement = specialCharacters [randomIndex];
-  password=password.unshift(randomElement);
-  return;
-};
 
-function numericCharacterFunction () {
-  var randomIndex = Math.floor(Math.random() * numericCharacters.length);
-  randomElement = numericCharacters [randomIndex];
-  password=password.unshift(randomElement);
-  return;
-};
-
-function lowerCaseCharacterFunction () {
-  var randomIndex = Math.floor(Math.random() * lowerCaseCharacters.length);
-  randomElement = lowerCaseCharacters [randomIndex];
-  password=password.unshift(randomElement);
-  return;
-};
-
-function upperCaseCharacterFunction () {
-  var randomIndex = Math.floor(Math.random() * upperCaseCharacters.length);
-  randomElement = upperCaseCharacters [randomIndex];
-  password=password.unshift(randomElement);
-  return;
-};
-
-}
 
 // Write password to the #password input
 function writePassword() {
